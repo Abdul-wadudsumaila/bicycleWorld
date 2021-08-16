3fe7aa4f28f60e3830799b2323283df12a14dfda
Abdul-wadudsumaila <88601172+Abdul-wadudsumaila@users.noreply.github.com>
2021-08-16 20:17:43 +0000

    Bicycle World Project

---
+++ bicycleWorld/bicycleWorld.bash
@@ -0,0 +1,30 @@
+$ pwd
+/home/ccuser/workspace/bicycle-world-ii
+$ ls
+brands.txt  freight  mountain  racing
+$ cd freight
+$ ls
+messenger  porteur
+$ cd porteur
+$ cd ../..
+$ pwd
+/home/ccuser/workspace/bicycle-world-ii
+$ cd mountain/downhill
+$ pwd
+/home/ccuser/workspace/bicycle-world-ii/mountain/downhill
+$ mkdir dirt.txt
+$ mkdir mud.txt
+$ ls
+dirt.txt  heavyweight  lightweight  mud.txt
+$ mkdir safety
+$ pwd
+/home/ccuser/workspace/bicycle-world-ii/mountain/downhill
+$ cd ../..
+$ pwd
+/home/ccuser/workspace/bicycle-world-ii
+$ ls
+brands.txt  freight  mountain  racing
+$ mkdir bmx
+$ touch bmx/tricks.txt
+$ ls
+bmx  brands.txt  freight  mountain  racing
